9239f17bd908c9a2201f31f1447db16074c86122
Jason Devaraj <jdevaraj01@gmail.com>
2025-02-19 23:24:26 -0500

    Finished logic.

--- app.cpp
+++ app.cpp
@@ -14,26 +14,25 @@ void userMemoryAllocation(int arr[]) {
     int totalPages = 100;
     int pagesUsed = 0;
     int startIndex = 0;
-    int endIndex = 0;
+    
     // Number of pages randomly generated (Size of proccess)
     while (pagesUsed < totalPages){
         int randomNum = (rand() % 30) + 1;
         int processSize = randomNum * 80;
-        int requiredPages = ceil(processSize / 160);
+        int requiredPages = ceil( (double) processSize / 160);
         pagesUsed += requiredPages;
+        int unusedSpace = (requiredPages*160) - processSize;
 
-        for(int i = startIndex; i < (startIndex + requiredPages) && i < 100; i++){
+        for(int i = startIndex; i < (startIndex + requiredPages) && i < totalPages; i++){
             arr[i] = currentProcess;
         }
+        
+        printf("%-15d %-25d %-30d %-10d\n", currentProcess, address, processSize, unusedSpace);
+        
         startIndex += requiredPages;
         currentProcess += 1;
-        
-        
+        address += requiredPages * 160;
     }
-    for(int i = 0; i < 100; i++){
-        printf("%d\n",arr[i]);
-    }
-    
     
 }
 
@@ -44,7 +43,9 @@ int main() {
     srand(time(NULL));
 
     // Declare an array of 100 integers. 
-    int emptyArr[100];
+    int emptyArr[100] = {0};
+    
+    std::cout << "\033[4mProcess ID: | Starting Memory Address: | Size of the Process MB  | Unused Space MB\033[0m" << std::endl;
 
     userMemoryAllocation(emptyArr);
 
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+![alt text](image.png)
+
 # Programming Assignment 1 | Operating Systems Course
 
 Author: Jason Devaraj
